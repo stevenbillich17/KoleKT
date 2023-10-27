22690ebf19e31ce5feb750841ebaac8726e55401
Steven Billich <stevenbillich17@gmail.com>
2023-10-27 21:45:33 +0300

    added basic run examples

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,31 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.tree.ParseTreeWalker
+import org.jetbrains.kotlin.spec.grammar.KotlinLexer
+import org.jetbrains.kotlin.spec.grammar.KotlinParser
+import org.jetbrains.kotlin.spec.grammar.KotlinParserBaseListener
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    runBasicTest()
+}
+
+fun runBasicTest() {
+    println("Basic Test:")
+    val basicTest = "var number: Double;"
+    val listener = object : KotlinParserBaseListener() {
+        override fun enterVariableDeclaration(ctx: KotlinParser.VariableDeclarationContext?) {
+            ctx?.let {
+                println("Variable name: ${it.simpleIdentifier().text}")
+                println("Variable type: ${it.type()?.text}")
+            }
+        }
+    }
+
+    val chStream = CharStreams.fromString(basicTest)
+    val tokens = CommonTokenStream(KotlinLexer(chStream))
+    val parser = KotlinParser(tokens)
+    val tree = parser.kotlinFile()
+    val walker = ParseTreeWalker()
+
+    walker.walk(listener, tree)
 }
