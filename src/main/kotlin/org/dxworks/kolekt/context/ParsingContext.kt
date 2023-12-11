7177eee557f5788433c171134f093a32babf3ad2
Steven Billich <stevenbillich17@gmail.com>
2023-12-11 19:23:36 +0200

    KT-57: added parsing context to the field listener

--- src/main/kotlin/org/dxworks/kolekt/context/ParsingContext.kt
+++ src/main/kotlin/org/dxworks/kolekt/context/ParsingContext.kt
@@ -5,35 +5,37 @@ import org.dxworks.kolekt.dtos.ClassDTO
 import org.dxworks.kolekt.dtos.MethodCallDTO
 
 class ParsingContext {
-    var shouldStop = false
-    var insideFunctionBody = false
-    var insidePropertyDeclaration = false
-    var insideDeclaration = false
-    var insideVariableDeclaration = false
-    var insideExpression = false
-    var insideInfixFunctionCall = false
-    var insidePrimaryExpression = false
-    var insideCallSuffix = false
-    var insideValueArgument = false
-    var insideAdditiveExpression = false
-    var insideMultiplicativeExpression = false
-    var insideAsExpression = false
-    var insidePrefixUnaryExpression = false
-    var insidePostfixUnaryExpression = false
-    var insidePostFixUnarySuffix = false
-    var insideNavigationSuffix = false
-    var insideFunctionParameters = false
-    var insideAnnotation = false
-    var insideSingleAnnotation = false
-    var insideUserType = false
-    var insideConstructorInvocation = false
+    var shouldStop: Boolean = false
+    var insideFunctionBody: Boolean = false
+    var insidePropertyDeclaration: Boolean = false
+    var insideDeclaration: Boolean = false
+    var insideVariableDeclaration: Boolean = false
+    var insideExpression: Boolean = false
+    var insideInfixFunctionCall: Boolean = false
+    var insidePrimaryExpression: Boolean = false
+    var insideCallSuffix: Boolean = false
+    var insideValueArgument: Boolean = false
+    var insideAdditiveExpression: Boolean = false
+    var insideMultiplicativeExpression: Boolean = false
+    var insideAsExpression: Boolean = false
+    var insidePrefixUnaryExpression: Boolean = false
+    var insidePostfixUnaryExpression: Boolean = false
+    var insidePostFixUnarySuffix: Boolean = false
+    var insideNavigationSuffix: Boolean = false
+    var insideFunctionParameters: Boolean = false
+    var insideAnnotation: Boolean = false
+    var insideSingleAnnotation: Boolean = false
+    var insideUserType: Boolean = false
+    var insideConstructorInvocation: Boolean = false
     var insideClassDeclaration: Boolean = false
-    var insidePrimaryConstructor = false
+    var insidePrimaryConstructor: Boolean = false
+    var insideSimpleIdentifier: Boolean = false
+    var containsColon: Boolean = false
 
-    var nameAlreadySetForMethod = false
-    var wasThereAnCallSuffix = false
-    var wasThereAnInfixFunctionCall = false
-    var wasThereAnNavigationSuffix = false
+    var nameAlreadySetForMethod: Boolean = false
+    var wasThereAnCallSuffix: Boolean = false
+    var wasThereAnInfixFunctionCall: Boolean = false
+    var wasThereAnNavigationSuffix: Boolean = false
 
     var valueName = ""
     var referenceName: String? = ""
