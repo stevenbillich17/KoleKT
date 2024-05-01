0188e23daa506898a335ce8e403ebd0710514965
Steven Billich <stevenbillich17@gmail.com>
2024-05-01 14:14:13 +0300

    KT-85: add doc for method and refactor appearence

--- src/main/kotlin/org/dxworks/kolekt/analyze/KoleClazzAnalyzer.kt
+++ src/main/kotlin/org/dxworks/kolekt/analyze/KoleClazzAnalyzer.kt
@@ -20,17 +20,23 @@ object KoleClazzAnalyzer {
         WOCMetricCalculator(),
     )
 
+    /**
+     * Analyzes a class and returns a JSON object with the metrics
+     * @param classFQN the fully qualified name of the class to analyze
+     * @param setInClass whether to set the metrics in the class or not
+     * @return a JSON object with the metrics
+     * @throws IllegalArgumentException if the class is not found
+     * Supported 14 metrics:
+     * NOM, NOPA, WMC, CC, CM, NProtM, AMW, CINT, CDISP, BOvR, WOC, DIT, HIT, NOC
+     */
     fun analyze(classFQN: String, setInClass: Boolean = false): JsonObject {
         val classDTO = FileController.findClassInFiles(classFQN) ?: throw IllegalArgumentException("Class not found")
-
         val metrics = metricsCalculators.map { it.calculateMetrics(classDTO, setInClass) }
-
         val classNameJson = JsonPrimitive(classFQN)
         val finalResult: Map<String, JsonElement> = buildMap {
-            put("class_fqn", classNameJson) // Add the class FQN to the final result
+            put("class_fqn", classNameJson)
             metrics.forEach { it.forEach { key, value -> put(key, value) } }
         }
-
         return JsonObject(finalResult)
     }
 }
